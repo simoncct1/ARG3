a8fe4c77a1446e043d7b043fe2830620ed9f70cc
simoncct1 <simoncouclet@gmail.com>
2024-01-05 23:46:41 +0100

    losemplojes

--- dist/accueil.dev.js
+++ dist/accueil.dev.js
@@ -2,17 +2,11 @@
 
 //influence
 var data = [{
-  year: 2010,
-  count: 0.27
-}, {
-  year: 2011,
-  count: 0.25
-}, {
   year: 2012,
-  count: 0.23
+  count: 0.13
 }, {
   year: 2013,
-  count: 0.35
+  count: 0.25
 }, {
   year: 2014,
   count: 0.56
@@ -43,6 +37,9 @@ var data = [{
 }, {
   year: 2023,
   count: 0.64
+}, {
+  year: 2024,
+  count: 0.74
 }];
 var influence = new Chart(document.getElementById('acquisitions'), {
   type: 'line',
@@ -60,6 +57,9 @@ var influence = new Chart(document.getElementById('acquisitions'), {
     },
     "responsive": true,
     "maintainAspectRatio": false,
+    pointRadius: 6,
+    pointBackgroundColor: "#BC6FF1",
+    borderWidth: 2,
     layout: {
       padding: 12
     },
@@ -114,7 +114,7 @@ var data2 = {
   labels: ['Missions réussies', 'Missions en cours', 'Missions échouées'],
   datasets: [{
     label: 'Missions de la F.N.D.P',
-    data: [84, 12, 8],
+    data: [61, 12, 8],
     backgroundColor: ['#52057B', '#892CDC', '#BC6FF1'],
     hoverOffset: 4
   }]
@@ -171,7 +171,7 @@ var pourcentage = new Chart(document.getElementById('pourcentage'), {
       },
       title: {
         display: true,
-        text: 'Missions de la F.N.D.P (%)',
+        text: 'Historique de missions (%)',
         color: "#FAFAFA",
         font: {
           size: 18,
@@ -183,5 +183,71 @@ var pourcentage = new Chart(document.getElementById('pourcentage'), {
     layout: {
       padding: 12
     }
+  }
+}); // recrues
+
+var mixedChart = new Chart(document.getElementById('recrues'), {
+  data: {
+    datasets: [{
+      type: 'bar',
+      label: 'Nouveaux membres',
+      data: [12, 8, 28, 8, 2, 0, 14, 1, 11, 20, 19, 34],
+      backgroundColor: "#52057B"
+    }, {
+      type: 'bar',
+      label: 'Total de membres',
+      data: [12, 20, 41, 27, 34, 20, 23, 17, 25, 36, 52, 79],
+      backgroundColor: "#BC6FF1"
+    }],
+    labels: ['1910', '1920', '1930', '1940', '1950', '1960', '1970', '1980', '1990', '2000', '2010', '2020']
+  },
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          color: "#FAFAFA",
+          font: {
+            size: 14,
+            family: 'Montserrat'
+          }
+        }
+      }
+    },
+    layout: {
+      padding: 5
+    },
+    scales: {
+      y: {
+        grid: {
+          display: false
+        },
+        ticks: {
+          color: "#FAFAFA",
+          font: {
+            size: 12
+          },
+          stepSize: 10,
+          beginAtZero: true
+        }
+      },
+      x: {
+        grid: {
+          display: false
+        },
+        ticks: {
+          color: "#FAFAFA",
+          font: {
+            size: 12
+          },
+          stepSize: 10,
+          beginAtZero: true
+        }
+      }
+    },
+    max: 90,
+    borderWidth: 2,
+    borderColor: "#FAFAFA",
+    responsive: true,
+    maintainAspectRatio: false
   }
 });
